7e5de0408392ea2e7a883e265a68c8f0d16f85b9
Yana Makogon <yana.mak89@gmail.com>
2024-05-21 16:54:49 +0200

    Tasks for Arrays and Indexers done

---
+++ Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+// Індексатори.
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary dictionary = new Dictionary();
+
+        Console.WriteLine(dictionary["книга"]);
+        Console.WriteLine(dictionary["дім"]);
+        Console.WriteLine(dictionary["ручка"]);
+        Console.WriteLine(dictionary["стол"]);
+        Console.WriteLine(dictionary["олівець"]);
+        Console.WriteLine(dictionary["яблуко"]);
+        Console.WriteLine(dictionary["сонце"]);
+
+        Console.WriteLine(new string('-', 20));
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(dictionary[i]);
+        }
+                        
+        // Delay.
+        Console.ReadKey();
+    }
+}
